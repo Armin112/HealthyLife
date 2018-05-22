3ac90ee0810227a2e94968267233820d2d815b8d
Armin112 <fehric.armin@gmail.com>
2018-05-22 11:55:47 +0200

    Register Functionality

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 const express = require('express');
 const app = express();
+var MongoClient = require('mongodb').MongoClient;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+var url = "mongodb://localhost:27017/";
+var md5 = require('md5');
+
 
 app.use('/', express.static('static'));
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
 
-app.listen(3200, () => console.log('Proba da vidimo da li radi'));
+
+
+app.post('/register', function(request, response){
+  var user = request.body;
+ 
+  db.collection('users').count({username: user.username}, function (err, count){ 
+    if(count>0){
+      return console.log(err);
+    }
+    else{
+      db.collection('users').save({'firstname': user.firstname, 'lastname': user.lastname, 'username': user.username
+      , 'email': user.email, 'password': md5(user.password)}, (err, result) => {
+if (err) return console.log(err);
+response.send('OK');
+      
+})
+    }
+}); 
+    
+ 
+});
+
+
+
+MongoClient.connect('mongodb://localhost:27017/healthylife', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3200, () => console.log('Example app listening on port 3200!'))
+})
--- static/controllers/register-controller.js
+++ static/controllers/register-controller.js
@@ -1,3 +1,25 @@
-function RegisterController($scope, $http){
-    console.log("Hello from login controller");
+function RegisterController($scope,$location, $http, $timeout){
+    console.log("Hello from controller");
+
+    var config = {headers:  {
+        'Authorization': 'Basic d2VudHdvcnRobWFuOkNoYW5nZV9tZQ==',
+        'Accept': 'application/json;odata=verbose',
+        "JWT" : localStorage.getItem('user')
+        }
+      };
+
+    $scope.add_user = function(){
+        $http.post('/register', $scope.user).then(function(response){
+          $scope.user = null;
+          $scope.message_success = "Congratulations, you are successfully created your profile. You will be redirected to login page now.";
+          $timeout(function(){ 
+            $location.path("/login"); 
+          },3000);
+        }, function(error){
+          console.log(error);
+        });
+      }
+
+      
+      
 }
